0f0e6775acf687b8d69e57238c091768751a457c
俞宏伟 <31241366+githowe@users.noreply.github.com>
2026-07-22 14:00:08 +0800

    添加中间接口库项目

---
+++ OCCTBridge/Base.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+
+#pragma comment(lib, "TKernel.lib")
+#pragma comment(lib, "TKMath.lib")
+#pragma comment(lib, "TKGeomBase.lib")
+#pragma comment(lib, "TKService.lib")
+#pragma comment(lib, "TKG2d.lib")
+#pragma comment(lib, "TKGeomAlgo.lib")
